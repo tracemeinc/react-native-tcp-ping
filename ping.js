fb27bded5e0dc1df554043d2c2a4ec4fa661bbfe
Christopher Fitzner <chris@traceme.com>
2018-04-11 16:27:47 -0700

    use native promise instead of promisify-es6

--- ping.js
+++ ping.js
@@ -21,81 +21,89 @@
 //SOFTWARE.
 
 var net = require('react-native-tcp');
-var promisify = require('promisify-es6');
 process.hrtime = require('browser-process-hrtime');
 
 var ping = function(options, callback) {
-    var i = 0;
-    var results = [];
-    options.address = options.address || 'localhost';
-    options.port = options.port || 80;
-    options.attempts = options.attempts || 10;
-    options.timeout = options.timeout || 5000;
-    options.minGood = options.minGood || 6;
+    return new Promise(function(resolve, reject) {
+        var i = 0;
+        var results = [];
+        options.address = options.address || 'localhost';
+        options.port = options.port || 80;
+        options.attempts = options.attempts || 10;
+        options.timeout = options.timeout || 5000;
+        options.minGood = options.minGood || 6;
 
-    var check = function(options, callback) {
-        if(i < options.attempts) {
-            connect(options, callback);
-        } else if (results.length < options.minGood) {
-            throw new Error('Not enough successful pings, ' + results.length + ' successful');
-        } else {
-            var avg = results.reduce(function(prev, curr) {
-                return prev + curr.time;
-            }, 0);
-            var max = results.reduce(function(prev, curr) {
-                return (prev > curr.time) ? prev : curr.time;
-            }, results[0].time);
-            var min = results.reduce(function(prev, curr) {
-                return (prev < curr.time) ? prev : curr.time;
-            }, results[0].time);
-            avg = avg / results.length;
-            var out = {
-                address: options.address,
-                port: options.port,
-                attempts: options.attempts,
-                avg: avg.toFixed(3),
-                max: max.toFixed(3),
-                min: min.toFixed(3),
-                results: results
-            };
-            callback(undefined, out);
-        }
-    };
+        var check = function(options, callback) {
+            if(i < options.attempts) {
+                connect(options, callback);
+            } else if (results.length < options.minGood) {
+                reject(new Error('Not enough successful pings, ' + results.length + ' successful'));
+            } else {
+                var avg = results.reduce(function(prev, curr) {
+                    return prev + curr.time;
+                }, 0);
+                var max = results.reduce(function(prev, curr) {
+                    return (prev > curr.time) ? prev : curr.time;
+                }, results[0].time);
+                var min = results.reduce(function(prev, curr) {
+                    return (prev < curr.time) ? prev : curr.time;
+                }, results[0].time);
+                avg = avg / results.length;
+                var out = {
+                    address: options.address,
+                    port: options.port,
+                    attempts: options.attempts,
+                    avg: avg.toFixed(3),
+                    max: max.toFixed(3),
+                    min: min.toFixed(3),
+                    results: results
+                };
+                resolve(out);
+            }
+        };
 
-    var connect = function(options, callback) {
-        var s = new net.Socket();
-        var start = process.hrtime();
-        s.connect(options.port, options.address, function() {
-            var time_arr = process.hrtime(start);
-            var time = (time_arr[0] * 1e9 + time_arr[1]) / 1e6;
-            results.push({ seq: i, time: time });
-            s.destroy();
-            i++;
-            check(options, callback);
-        });
-        s.on('error', function(e) {
-            s.destroy();
-            i++;
-            check(options, callback);
-        });
-        s.setTimeout(options.timeout, function() {
-            s.destroy();
-            i++;
-            check(options, callback);
-        });
-    };
-    connect(options, callback);
-};
+        var connect = function(options, callback) {
+            var s = new net.Socket();
+            var start = process.hrtime();
+            s.connect(options.port, options.address, function() {
+                var time_arr = process.hrtime(start);
+                var time = (time_arr[0] * 1e9 + time_arr[1]) / 1e6;
+                results.push({ seq: i, time: time });
+                s.destroy();
+                i++;
+                check(options, callback);
+            });
+            s.on('error', function(e) {
+                s.destroy();
+                i++;
+                check(options, callback);
+            });
+            s.setTimeout(options.timeout, function() {
+                s.destroy();
+                i++;
+                check(options, callback);
+            });
+        };
+        connect(options, callback);
+    });
+}
 
-module.exports.ping = promisify(ping);
+module.exports.ping = ping;
 
 var probe = function(address, port, callback) {
-    address = address || 'localhost';
-    port = port || 80;
-    ping({ address: address, port: port, attempts: 1, timeout: 1000 }, function(err, data) {
-        var available = data.min !== undefined;
-        callback(err, available);
+    return new Promise(function(resolve, reject) {
+        address = address || 'localhost';
+        port = port || 80;
+        ping({ address: address, port: port, attempts: 1, timeout: 1000 }, function(err, data) {
+            var available = data.min !== undefined;
+            if (err) {
+                reject(err);
+            }
+            else {
+                resolve(available);
+            }
+        });
     });
-};
+}
 
-module.exports.probe = promisify(probe);
+module.exports.probe = probe;
